9f55ae5fa0508c093e648c80cac44919908bc4dd
Simon Alberto Rodriguez Pacheco <saymon_set@hotmail.com>
2022-02-12 16:09:59 -0400

    Seccion23->177: Se resuelve el error de bags (bosas)  al hacer varios toMany a fetch eager, se coloca Set por List, pero no es recomenado. Es pobre el rendimiento. es  mejor hacer la consulta aparte para llenar los eager aparte pero por el cliente y no por todos los clientes

--- seccion23-jpa-hibernate-fetch-eager-lazy/2_fetch_onetomany_resultList/src/main/java/org/aguzman/hibernateapp/HibernateFetchResultList.java
+++ seccion23-jpa-hibernate-fetch-eager-lazy/2_fetch_onetomany_resultList/src/main/java/org/aguzman/hibernateapp/HibernateFetchResultList.java
@@ -10,7 +10,15 @@ public class HibernateFetchResultList {
     public static void main(String[] args) {
 
         EntityManager em = JpaUtil.getEntityManager();
+        /*Podemos optimizar el rendimieto de la consulta , manteniendo easy
+                en direcciones y usando esta consulta con left outer join fetch en direcciones*/
+        /*importante el fetch para poblar*/
+
+        /*Con disstinct, el cliente no se repite al tener tres direcciones o dos direccioes, solo aparecera
+                el cliente con el arreglo de direcciones y no repetidamente por cada direccion*/
         List<Cliente> clientes = em.createQuery("select distinct c from Cliente c left outer join fetch c.direcciones left outer join fetch c.detalle", Cliente.class).getResultList();
+        /*los terminados en many, son bolsas (bags) y si haces los finalizados en many a fetch tipo lazy y hay mas de 1,
+                dara error con bolsas en la corrida*/
 
         clientes.forEach(c -> System.out.println(c.getNombre() + ", direcciones: " + c.getDirecciones()));
         em.close();
