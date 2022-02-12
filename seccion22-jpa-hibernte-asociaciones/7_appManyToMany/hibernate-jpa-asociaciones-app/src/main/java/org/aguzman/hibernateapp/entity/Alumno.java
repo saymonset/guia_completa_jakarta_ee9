61ff277dfb7619623a7d68943b6b97446435045a
Simon Alberto Rodriguez Pacheco <saymon_set@hotmail.com>
2022-02-12 09:01:30 -0400

    Seccion22->170: Relacin many toMany, Solo cascade.persist, and cascade. merge, el cascade.remove se ignora

--- seccion22-jpa-hibernte-asociaciones/7_appManyToMany/hibernate-jpa-asociaciones-app/src/main/java/org/aguzman/hibernateapp/entity/Alumno.java
+++ seccion22-jpa-hibernte-asociaciones/7_appManyToMany/hibernate-jpa-asociaciones-app/src/main/java/org/aguzman/hibernateapp/entity/Alumno.java
@@ -16,6 +16,15 @@ public class Alumno {
     private String nombre;
     private String apellido;
 
+    /*La clase alumno, va a ser la principal mientras que curso va
+     a ser la secundaria o dependiente de alumno*/
+    /*Como es la principal debemos manejar el cascade*/
+    /*Si ingresamos un alumno con sus cursos, va a ingresar todo en cscada*/
+    /*No colocamos cascade en all, porque si colocamos all, va  a incluir el remove*/
+
+    /*No colocamos cascade.all , porque tendria  a remove, y si eliminamos un alucmno, va aeliminar
+    sus cursos tambien y va  agenerar un error po constraint a l tratar de eliminar cursos
+    que lo tienen otros alumnos*/
     @ManyToMany(cascade = {CascadeType.PERSIST, CascadeType.MERGE})
     private List<Curso> cursos;
 
@@ -24,6 +33,7 @@ public class Alumno {
     }
 
     public Alumno(String nombre, String apellido) {
+        //Inicializa el contructor default y este a su ves inicializa curso
         this();
         this.nombre = nombre;
         this.apellido = apellido;
