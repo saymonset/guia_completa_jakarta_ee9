343f062d2cef9be48016afef0c3f3fbc7cb15419
Simon Alberto Rodriguez Pacheco <saymon_set@hotmail.com>
2022-04-22 05:51:07 -0400

    Seccion34-primefaces

---
+++ seccion34-primefaces/4_webapp-jsf3-primefaces[modal_ajax]/webapp-jsf3-primefaces/src/main/java/org/aguzman/webapp/jsf3/services/ProductoServiceImpl.java
@@ -0,0 +1,56 @@
+package org.aguzman.webapp.jsf3.services;
+
+import jakarta.ejb.Stateless;
+import jakarta.inject.Inject;
+import org.aguzman.webapp.jsf3.entities.Categoria;
+import org.aguzman.webapp.jsf3.entities.Producto;
+import org.aguzman.webapp.jsf3.repositories.CrudRepository;
+import org.aguzman.webapp.jsf3.repositories.ProductoRepository;
+
+import java.util.List;
+import java.util.Optional;
+
+@Stateless
+public class ProductoServiceImpl implements ProductoService {
+
+    @Inject
+    private ProductoRepository repository;
+
+    @Inject
+    private CrudRepository<Categoria> categoriaRepository;
+
+    @Override
+    public List<Producto> listar() {
+        return repository.listar();
+    }
+
+    @Override
+    public Optional<Producto> porId(Long id) {
+        return Optional.ofNullable(repository.porId(id));
+    }
+
+    @Override
+    public void guardar(Producto producto) {
+        repository.guardar(producto);
+    }
+
+    @Override
+    public void eliminar(Long id) {
+        repository.eliminar(id);
+    }
+
+    @Override
+    public List<Categoria> listarCategorias() {
+        return categoriaRepository.listar();
+    }
+
+    @Override
+    public Optional<Categoria> porIdCategoria(Long id) {
+        return Optional.ofNullable(categoriaRepository.porId(id));
+    }
+
+    @Override
+    public List<Producto> buscarPorNombre(String nombre) {
+        return repository.buscarPorNombre(nombre);
+    }
+}
